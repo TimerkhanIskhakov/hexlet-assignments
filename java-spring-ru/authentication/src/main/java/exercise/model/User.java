72f380d86fda78a08bbfe32f662d71d3420c722a
@hexlet/cli <support@hexlet.io>
2023-02-13 22:19:18 +0300

    submit java-spring-ru/authentication

---
+++ java-spring-ru/authentication/src/main/java/exercise/model/User.java
@@ -0,0 +1,59 @@
+package exercise.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    // Имя пользователя, которое будет использоваться при аутентификации
+    private String username;
+
+    private String email;
+
+    // Поле, которое содержит пароль пользователя
+    // С этим паролем будет сравниваться пароль, введённый при аутентификации
+    private String password;
+
+    @Transient
+    private List<SimpleGrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
